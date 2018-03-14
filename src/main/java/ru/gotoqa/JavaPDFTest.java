af45611b620a566a83f0abcf7f3cdeccfcbc826d
MuflikhunovRR <discus12@mail.ru>
2018-03-14 21:34:49 +0300

    pdf2text example commit

--- src/main/java/ru/gotoqa/JavaPDFTest.java
+++ src/main/java/ru/gotoqa/JavaPDFTest.java
@@ -7,7 +7,7 @@ public class JavaPDFTest {
     public static void main(String[] args) throws IOException {
 
         PDFManager pdfManager = new PDFManager();
-        pdfManager.setFilePath("D:\\JAVA\\Java_SRC\\pdf2text\\src\\main\\resources\\pdffile1.pdf");
+        pdfManager.setFilePath("D:\\JAVA\\Java_SRC\\pdf2text\\src\\main\\resources\\pdffile2.pdf");
         System.out.println(pdfManager.ToText().hashCode());
     }
 }
